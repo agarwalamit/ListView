bf692aa5be36508191f2ac8e0d29aaafacd74b85
Amit Agarwal <amit.agarwal@flipkart.com>
2014-04-22 11:59:50 +0530

    simple implementation of Listview working fine with height of lv as match parent....doesnt work well with height of lv as warpcontent....with wrapcontent getview get called more number of times than the size of getCount

--- src/com/example/listview/MainActivity.java
+++ src/com/example/listview/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.listview;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -10,13 +17,43 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		final ListView listview = (ListView) findViewById(R.id.lv);
+		String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+				"Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+				"Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+				"OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+				"Android", "iPhone", "WindowsMobile" };
+
+		final ArrayList<String> list = new ArrayList<String>();
+		for (int i = 0; i < values.length; ++i) {
+			list.add(values[i]);
+		}
+		final MyArrayAdapter adapter = new MyArrayAdapter(this,
+				android.R.layout.simple_list_item_1, list);
+		listview.setAdapter(adapter);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private class MyArrayAdapter extends ArrayAdapter<String> {
+		
+		int i=0;
+		
+		public MyArrayAdapter(Context context, int textViewResourceId,
+				List<String> objects) {
+			super(context, textViewResourceId, objects);
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent){
+			i++;
+			System.out.println("getView called-------------"+i);
+			return super.getView(position, convertView, parent);
+		}
+		
+		@Override
+		public int getCount(){
+			System.out.println("getCount called " +super.getCount());
+			return super.getCount();
+		}
 	}
-
-}
+} 
